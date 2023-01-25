5d918083160c955ffafe78cdc017b83b20670331
Alexey <alexeyinshakov992@gmail.com>
2023-01-25 11:27:57 +0300

    block-why is done for portfolio

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,34 @@
 import * as flsFunctions from "./modules/functions.js";
 import { rightMenu } from "./modules/right-menu.js";
 import { Fancybox, Carousel, Panzoom } from "@fancyapps/ui"
+import Swiper, { Pagination } from 'swiper';
 
 window.addEventListener('load', (event) => {
    flsFunctions.isWebp();
    rightMenu()
+
+   const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      // direction: 'horizontal',
+      // loop: true,
+      modules: [Pagination],
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      // navigation: {
+      //   nextEl: '.swiper-button-next',
+      //   prevEl: '.swiper-button-prev',
+      // },
+    
+      // And if we need scrollbar
+      // scrollbar: {
+      //   el: '.swiper-scrollbar',
+      // },
+    });
+
 });
 
 
